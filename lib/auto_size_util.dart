026f542d10c921a0aa6354de333aaad124d3b74c
niezhiyang <nie.zhiyang@immomo.com>
2021-08-24 18:11:08 +0800

    添加获取真实的高度方法，和 Text 是否随着系统更改

--- lib/auto_size_util.dart
+++ lib/auto_size_util.dart
@@ -13,10 +13,14 @@ class AutoSizeUtil {
 
   static Size _screenSize = Size.zero;
 
+  static bool _autoTextSize = true;
+
   /// 如果是横屏 就以高度为基准
   /// 如果是竖屏 就以宽度为基准
-  static void setStandard(double standard) {
+  /// 是否随着系统的文字大小而改变，默认是改变
+  static void setStandard(double standard, {bool isAutoTextSize = true}) {
     _screenStandard = standard;
+    _autoTextSize = isAutoTextSize;
   }
 
   /// 根据设置 的 宽度 来得到 devicePixelRatio
@@ -62,18 +66,28 @@ class AutoSizeUtil {
     return _screenSize;
   }
 
-
   static Widget appBuilder(BuildContext context, Widget? widget) {
     return MediaQuery(
       // 这里如果设置 textScaleFactor = 1.0 ，就不会随着系统字体大小去改变了
       data: MediaQuery.of(context).copyWith(
           size: Size(AutoSizeUtil._screenWidth, AutoSizeUtil._screenHeight),
           devicePixelRatio: AutoSizeUtil._devicePixelRatio,
-          textScaleFactor: 1.0),
-      child: Theme(
-        data: Theme.of(context).copyWith(),
-        child: widget!,
-      ),
+          textScaleFactor:
+              _autoTextSize ? MediaQuery.of(context).textScaleFactor : 1.0),
+      child: _adapterTheme(context, widget),
+    );
+  }
+
+  /// 获取真正的大小，比如 kToolbarHeight kBottomNavigationBarHeight
+  static double getRealSize(double size) {
+    return size / (_devicePixelRatio / window.devicePixelRatio);
+  }
+
+  static _adapterTheme(BuildContext context, Widget? widget) {
+
+    return Theme(
+      data: Theme.of(context).copyWith(),
+      child: widget!,
     );
   }
 }
