88df7520aa7312152db8b130a2da23e5d5d72740
渣渣辉 <1@fxcdev.com>
2022-01-26 14:55:20 +0800

    适配桌面版（桌面版本不启用自动缩放，采用原始比例）

--- lib/binding.dart
+++ lib/binding.dart
@@ -22,10 +22,16 @@ class AutoWidgetsFlutterBinding extends WidgetsFlutterBinding {
 
   @override
   ViewConfiguration createViewConfiguration() {
-    return ViewConfiguration(
-      size: AutoSizeUtil.getSize(),
-      devicePixelRatio: AutoSizeUtil.getDevicePixelRatio(),
-    );
+    var queryData = MediaQueryData.fromWindow(window);
+    if (queryData.size.shortestSide < 600) {
+      window.onPointerDataPacket = _handlePointerDataPacket;
+      return ViewConfiguration(
+        size: AutoSizeUtil.getSize(),
+        devicePixelRatio: AutoSizeUtil.getDevicePixelRatio(),
+      );
+    } else {
+      return super.createViewConfiguration();
+    }
   }
 
   /// 因为修改了 devicePixelRatio ，得适配点击事件  GestureBinding
@@ -44,10 +50,13 @@ class AutoWidgetsFlutterBinding extends WidgetsFlutterBinding {
   final Queue<PointerEvent> _pendingPointerEvents = Queue<PointerEvent>();
 
   void _handlePointerDataPacket(PointerDataPacket packet) {
+    var width = MediaQueryData.fromWindow(window).size.shortestSide;
     _pendingPointerEvents.addAll(PointerEventConverter.expand(
         packet.data,
         // 适配事件的转换比率,采用我们修改的
-        AutoSizeUtil.getDevicePixelRatio()));
+        width < 600
+            ? AutoSizeUtil.getDevicePixelRatio()
+            : window.devicePixelRatio));
     if (!locked) _flushPointerEventQueue();
   }
 
